0a85c29d80b96f54decd9e87c8804fbebd47c39a
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-02-19 20:31:11 +0100

    Create PatientController.java
    -Class holds the logic to catch and execute requests to "/patients" endpoints

---
+++ src/main/java/com/example/demo/controller/PatientController.java
@@ -0,0 +1,44 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Patient;
+import com.example.demo.service.PatientService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/patients")
+public class PatientController {
+    private final PatientService patientService;
+
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @GetMapping
+    public List<Patient> getPatients() {
+        return patientService.getAllPatients();
+    }
+
+    @GetMapping("/{email}")
+    public Patient getPatient(@PathVariable("email") String email) {
+        return patientService.getPatient(email);
+    }
+
+    @PostMapping
+    public Patient createPatient(@RequestBody Patient patient) {
+        return patientService.createPatient(patient);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{email}")
+    public void deletePatient(@PathVariable("email") String email) {
+        patientService.deletePatient(email);
+    }
+
+    @PutMapping("/{email}")
+    public Patient editPatient(@PathVariable("email") String email, @RequestBody Patient newPatientInfo) {
+        return patientService.editPatient(email, newPatientInfo);
+    }
+}
