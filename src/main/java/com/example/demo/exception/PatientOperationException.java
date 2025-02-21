95fa15c508183609fecfaeb8aa2654dad6e656be
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-02-21 18:56:34 +0100

    Delete PatientOperationException.java

--- src/main/java/com/example/demo/exception/PatientOperationException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.exception;
-
-public class PatientOperationException extends RuntimeException {
-    public PatientOperationException(String message) {
-        super(message);
-    }
-}
