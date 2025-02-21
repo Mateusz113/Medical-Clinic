4de229b75f5dd7a38e7886c3d64b8c65c0195d42
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-02-21 12:48:29 +0100

    Update PatientService.java
    Delegated all patient manipulation logic to repository

--- src/main/java/com/example/demo/service/PatientService.java
+++ src/main/java/com/example/demo/service/PatientService.java
@@ -1,14 +1,10 @@
 package com.example.demo.service;
 
-import com.example.demo.exception.PatientCreationException;
-import com.example.demo.exception.PatientDeletionException;
-import com.example.demo.exception.PatientEditionException;
 import com.example.demo.model.Patient;
 import com.example.demo.repository.PatientRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class PatientService {
@@ -23,42 +19,18 @@ public class PatientService {
     }
 
     public Patient getPatient(String email) {
-        Optional<Patient> patientOptional = patientRepository.getPatient(email);
-        return patientOptional.orElseThrow();
+        return patientRepository.getPatient(email);
     }
 
     public Patient createPatient(Patient patient) {
-        if (patientRepository.addPatient(patient)) {
-            return patient;
-        } else {
-            throw new PatientCreationException("Failed to create new patient.");
-        }
+        return patientRepository.addPatient(patient);
     }
 
     public void deletePatient(String email) {
-        if (!patientRepository.deletePatient(email)) {
-            throw new PatientDeletionException("Failed to delete patient with given email.");
-        }
+        patientRepository.deletePatient(email);
     }
 
-    public Patient editPatient(String email, Patient newPatientInfo) {
-        Optional<Patient> patientToEdit = patientRepository.getPatient(email);
-        patientToEdit.ifPresentOrElse(
-                patient -> updatePatientInfo(patient, newPatientInfo),
-                () -> {
-                    throw new PatientEditionException("Failed to edit patient information.");
-                }
-        );
-        return newPatientInfo;
-    }
-
-    private void updatePatientInfo(Patient patientToUpdate, Patient newPatientInfo) {
-        patientToUpdate.setEmail(newPatientInfo.getEmail());
-        patientToUpdate.setPassword(newPatientInfo.getPassword());
-        patientToUpdate.setIdCardNo(newPatientInfo.getIdCardNo());
-        patientToUpdate.setFirstName(newPatientInfo.getFirstName());
-        patientToUpdate.setLastName(newPatientInfo.getLastName());
-        patientToUpdate.setPhoneNumber(newPatientInfo.getPhoneNumber());
-        patientToUpdate.setBirthday(newPatientInfo.getBirthday());
+    public Patient editPatient(String email, Patient newPatientData) {
+        return patientRepository.updatePatient(email, newPatientData);
     }
 }
