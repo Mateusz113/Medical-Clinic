6f0e1ab2ef4b6f5d17f49c82d91b67876e1fa8a9
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-02-21 12:51:31 +0100

    Update PatientRepository.java
    Added a function to edit user
    Updated functions to handle exceptions and return copies of objects

--- src/main/java/com/example/demo/repository/PatientRepository.java
+++ src/main/java/com/example/demo/repository/PatientRepository.java
@@ -1,11 +1,11 @@
 package com.example.demo.repository;
 
+import com.example.demo.exception.PatientOperationException;
 import com.example.demo.model.Patient;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public class PatientRepository {
@@ -16,18 +16,53 @@ public class PatientRepository {
     }
 
     public List<Patient> getAllPatients() {
-        return patients;
+        return new ArrayList<>(patients);
     }
 
-    public Optional<Patient> getPatient(String email) {
-        return patients.stream().filter(patient -> patient.getEmail().equals(email)).findFirst();
+    public Patient getPatient(String email) {
+        return patients.stream()
+                .filter(patient -> patient.getEmail().equals(email))
+                .findFirst()
+                .map(patient -> new Patient(
+                        patient.getEmail(),
+                        patient.getPassword(),
+                        patient.getIdCardNo(),
+                        patient.getFirstName(),
+                        patient.getLastName(),
+                        patient.getPhoneNumber(),
+                        patient.getBirthday()
+                ))
+                .orElseThrow(() -> new PatientOperationException("Could not retrieve patient. Patient with given email does not exist."));
     }
 
-    public boolean addPatient(Patient patient) {
-        return patients.add(patient);
+    public Patient addPatient(Patient patient) {
+        boolean emailIsAvailable = patients.stream()
+                .noneMatch(existingPatient -> patient.getEmail().equals(existingPatient.getEmail()));
+        if (!emailIsAvailable) {
+            throw new PatientOperationException("Could not create a new patient. Patient with given email already exists.");
+        }
+        patients.add(patient);
+        return patient;
     }
 
-    public boolean deletePatient(String email) {
-        return patients.removeIf(patient -> patient.getEmail().equals(email));
+    public void deletePatient(String email) {
+        if (!patients.removeIf(patient -> patient.getEmail().equals(email))) {
+            throw new PatientOperationException("Could not delete patient. Patient with given email does not exist.");
+        }
+    }
+
+    public Patient updatePatient(String email, Patient newPatientData) {
+        Patient patientToUpdate = patients.stream()
+                .filter(patient -> patient.getEmail().equals(email))
+                .findFirst()
+                .orElseThrow(() -> new PatientOperationException("Could not edit patient. Patient with given email does not exist."));
+        patientToUpdate.setEmail(newPatientData.getEmail());
+        patientToUpdate.setPassword(newPatientData.getPassword());
+        patientToUpdate.setIdCardNo(newPatientData.getIdCardNo());
+        patientToUpdate.setFirstName(newPatientData.getFirstName());
+        patientToUpdate.setLastName(newPatientData.getLastName());
+        patientToUpdate.setPhoneNumber(newPatientData.getPhoneNumber());
+        patientToUpdate.setBirthday(newPatientData.getBirthday());
+        return getPatient(email);
     }
 }
