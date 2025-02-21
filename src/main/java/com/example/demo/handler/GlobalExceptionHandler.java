62604a94662b312b232db456e3b6e1504ab19570
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-02-21 18:57:01 +0100

    Create GlobalExceptionHandler.java
    Added handler for app exceptions

---
+++ src/main/java/com/example/demo/handler/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.example.demo.handler;
+
+import com.example.demo.exception.PatientAlreadyExistsException;
+import com.example.demo.exception.PatientNotFoundException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(exception = PatientNotFoundException.class)
+    public ResponseEntity<Object> handlePatientNotFoundException(RuntimeException ex, WebRequest request) {
+        String body = ex.getMessage();
+        return handleExceptionInternal(ex, body, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
+
+    @ExceptionHandler(exception = PatientAlreadyExistsException.class)
+    public ResponseEntity<Object> handlePatientAlreadyExistsException(RuntimeException ex, WebRequest request) {
+        String body = ex.getMessage();
+        return handleExceptionInternal(ex, body, new HttpHeaders(), HttpStatus.CONFLICT, request);
+    }
+}
