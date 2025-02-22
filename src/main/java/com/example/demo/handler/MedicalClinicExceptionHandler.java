c5fbf8d1ad0b1acce3c1b4147ba79cd9a239a759
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-02-22 14:06:52 +0100

    Update MedicalClinicExceptionHandler.java
    Updated class name

--- src/main/java/com/example/demo/handler/MedicalClinicExceptionHandler.java
+++ src/main/java/com/example/demo/handler/MedicalClinicExceptionHandler.java
@@ -11,7 +11,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+public class MedicalClinicExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(exception = PatientNotFoundException.class)
     public ResponseEntity<Object> handlePatientNotFoundException(RuntimeException ex, WebRequest request) {
         String body = ex.getMessage();
