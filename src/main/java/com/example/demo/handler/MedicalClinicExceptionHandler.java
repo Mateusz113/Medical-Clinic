cb523ba9c80d0c6175b216ad61ab6c95d0e775c8
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-02-23 19:29:44 +0100

    Update MedicalClinicExceptionHandler.java
    Changed logic to handle WebException

--- src/main/java/com/example/demo/handler/MedicalClinicExceptionHandler.java
+++ src/main/java/com/example/demo/handler/MedicalClinicExceptionHandler.java
@@ -1,9 +1,8 @@
 package com.example.demo.handler;
 
-import com.example.demo.exception.PatientAlreadyExistsException;
-import com.example.demo.exception.PatientNotFoundException;
+import com.example.demo.exception.WebException;
+import com.example.demo.model.ErrorMessage;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,15 +11,9 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @ControllerAdvice
 public class MedicalClinicExceptionHandler extends ResponseEntityExceptionHandler {
-    @ExceptionHandler(exception = PatientNotFoundException.class)
-    public ResponseEntity<Object> handlePatientNotFoundException(RuntimeException ex, WebRequest request) {
-        String body = ex.getMessage();
-        return handleExceptionInternal(ex, body, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
-    }
-
-    @ExceptionHandler(exception = PatientAlreadyExistsException.class)
-    public ResponseEntity<Object> handlePatientAlreadyExistsException(RuntimeException ex, WebRequest request) {
-        String body = ex.getMessage();
-        return handleExceptionInternal(ex, body, new HttpHeaders(), HttpStatus.CONFLICT, request);
+    @ExceptionHandler(exception = WebException.class)
+    public ResponseEntity<Object> handleWebException(WebException ex, WebRequest request) {
+        ErrorMessage body = new ErrorMessage(ex.getMessage(), ex.getStatusCode(), ex.getDate());
+        return handleExceptionInternal(ex, body, new HttpHeaders(), ex.getStatusCode(), request);
     }
 }
