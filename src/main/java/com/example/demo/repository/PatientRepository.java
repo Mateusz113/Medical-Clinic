c99cd1a07f3fd5650618bf825a5af49375d904a7
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-03-03 21:47:44 +0100

    Update PatientRepository.java
    Reworked the repository to be a JPARepository

--- src/main/java/com/example/demo/repository/PatientRepository.java
+++ src/main/java/com/example/demo/repository/PatientRepository.java
@@ -1,77 +1,35 @@
 package com.example.demo.repository;
 
-import com.example.demo.exception.PatientAlreadyExistsException;
-import com.example.demo.exception.PatientIllegalArgumentException;
-import com.example.demo.exception.PatientNotFoundException;
 import com.example.demo.model.Patient;
-import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.time.OffsetDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 
 @Repository
-@RequiredArgsConstructor
-public class PatientRepository {
-    private final List<Patient> patients;
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+    Optional<Patient> getPatientByEmail(String email);
 
-    public List<Patient> getAllPatients() {
-        return new ArrayList<>(patients);
-    }
+    int deletePatientByEmail(String email);
 
-    public Optional<Patient> getPatient(String email) {
-        Optional<Patient> existingPatient = getPatientReference(email);
-        return existingPatient.map(Patient::copy);
-    }
+    @Modifying
+    @Query("""
+            update PATIENTS p
+            set
+            p.email = :#{#patientData.email},
+            p.password = :#{#patientData.password},
+            p.firstName = :#{#patientData.firstName},
+            p.lastName = :#{#patientData.lastName},
+            p.phoneNumber = :#{#patientData.phoneNumber},
+            p.birthday = :#{#patientData.birthday}
+            where lower(p.email) = lower(:email)
+            """)
+    int updatePatient(@Param("email") String email, @Param("patientData") Patient patientData);
 
-    public Patient addPatient(Patient patient) {
-        boolean emailIsAvailable = patients.stream()
-                .noneMatch(existingPatient -> patient.getEmail().equals(existingPatient.getEmail()));
-        if (!emailIsAvailable) {
-            throw new PatientAlreadyExistsException("Patient with email: %s already exists".formatted(patient.getEmail()), OffsetDateTime.now());
-        }
-        patients.add(patient);
-        return patient;
-    }
-
-    public boolean deletePatient(String email) {
-        return patients.removeIf(patient -> patient.getEmail().equals(email));
-    }
-
-    public Patient updatePatient(String email, Patient newPatientData) {
-        Patient patientToUpdate = getPatientReference(email)
-                .orElseThrow(() -> new PatientNotFoundException("Patient with email: %s does not exist.".formatted(email), OffsetDateTime.now()));
-        if (!patientToUpdate.getIdCardNo().equals(newPatientData.getIdCardNo())) {
-            throw new PatientIllegalArgumentException("ID card number cannot be changed.", OffsetDateTime.now());
-        }
-        boolean isNewPatientEmailAvailable = patients.stream()
-                .noneMatch(patient -> patient.getEmail().equals(newPatientData.getEmail()));
-        if (!isNewPatientEmailAvailable && !Objects.equals(email, newPatientData.getEmail())) {
-            throw new PatientIllegalArgumentException("Email: %s is already taken.".formatted(newPatientData.getEmail()), OffsetDateTime.now());
-        }
-        patientToUpdate.setEmail(newPatientData.getEmail());
-        patientToUpdate.setPassword(newPatientData.getPassword());
-        patientToUpdate.setIdCardNo(newPatientData.getIdCardNo());
-        patientToUpdate.setFirstName(newPatientData.getFirstName());
-        patientToUpdate.setLastName(newPatientData.getLastName());
-        patientToUpdate.setPhoneNumber(newPatientData.getPhoneNumber());
-        patientToUpdate.setBirthday(newPatientData.getBirthday());
-        return patientToUpdate.copy();
-    }
-
-    public Patient updatePatientPassword(String email, String password) {
-        Patient patientToUpdate = getPatientReference(email)
-                .orElseThrow(() -> new PatientNotFoundException("Patient with email: %s does not exist.".formatted(email), OffsetDateTime.now()));
-        patientToUpdate.setPassword(password);
-        return patientToUpdate.copy();
-    }
-
-    private Optional<Patient> getPatientReference(String email) {
-        return patients.stream()
-                .filter(patient -> patient.getEmail().equals(email))
-                .findFirst();
-    }
+    @Modifying
+    @Query("update PATIENTS p set p.password = :password where lower(p.email) = lower(:email) ")
+    int updatePatientPassword(@Param("email") String email, @Param("password") String password);
 }
