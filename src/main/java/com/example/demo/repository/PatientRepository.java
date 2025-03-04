af8e21d38a40b7ef7d24f46673fa6673c0687285
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-03-04 20:19:36 +0100

    Update PatientRepository.java
    Removed redundant methods

--- src/main/java/com/example/demo/repository/PatientRepository.java
+++ src/main/java/com/example/demo/repository/PatientRepository.java
@@ -2,9 +2,6 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -12,26 +9,6 @@ import java.util.Optional;
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Long> {
     Optional<Patient> findByEmail(String email);
-
-    int deleteByEmail(String email);
-
-    @Modifying
-    @Query("""
-            update PATIENTS p
-            set
-            p.email = :#{#patientData.email},
-            p.password = :#{#patientData.password},
-            p.firstName = :#{#patientData.firstName},
-            p.lastName = :#{#patientData.lastName},
-            p.phoneNumber = :#{#patientData.phoneNumber},
-            p.birthday = :#{#patientData.birthday}
-            where lower(p.email) = lower(:email)
-            """)
-    int update(@Param("email") String email, @Param("patientData") Patient patientData);
-
-    @Modifying
-    @Query("update PATIENTS p set p.password = :password where lower(p.email) = lower(:email) ")
-    int updatePassword(@Param("email") String email, @Param("password") String password);
 
     boolean existsByEmail(String email);
 }
