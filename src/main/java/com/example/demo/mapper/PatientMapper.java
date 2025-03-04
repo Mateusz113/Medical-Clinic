b97bfd5af4ce988602a727b1ac98caceeb1de381
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-03-04 16:03:06 +0100

    Update PatientMapper.java
    fixed mapper to add default null when mapping a dto to patient entity

--- src/main/java/com/example/demo/mapper/PatientMapper.java
+++ src/main/java/com/example/demo/mapper/PatientMapper.java
@@ -4,10 +4,12 @@ import com.example.demo.model.FullPatientDataDTO;
 import com.example.demo.model.Patient;
 import com.example.demo.model.PatientDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface PatientMapper {
     PatientDTO toDTO(Patient patient);
 
+    @Mapping(target = "id", expression = "java(null)")
     Patient toEntity(FullPatientDataDTO patientData);
 }
