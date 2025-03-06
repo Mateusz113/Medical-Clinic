e4b0c7892d61a8bd771b0b26e71505a206899d9c
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-03-06 13:09:24 +0100

    Moved Patient exceptions to new package

--- src/main/java/com/example/demo/exception/patient/PatientIllegalArgumentException.java
+++ src/main/java/com/example/demo/exception/patient/PatientIllegalArgumentException.java
@@ -1,10 +1,11 @@
-package com.example.demo.exception;
+package com.example.demo.exception.patient;
 
+import com.example.demo.exception.WebException;
 import org.springframework.http.HttpStatus;
 
 import java.time.OffsetDateTime;
 
-public class PatientIllegalArgumentException extends WebException{
+public class PatientIllegalArgumentException extends WebException {
     public PatientIllegalArgumentException(String message, OffsetDateTime date) {
         super(message, HttpStatus.BAD_REQUEST, date);
     }
--- src/main/java/com/example/demo/exception/patient/PatientNotFoundException.java
+++ src/main/java/com/example/demo/exception/patient/PatientNotFoundException.java
@@ -1,5 +1,6 @@
-package com.example.demo.exception;
+package com.example.demo.exception.patient;
 
+import com.example.demo.exception.WebException;
 import org.springframework.http.HttpStatus;
 
 import java.time.OffsetDateTime;
