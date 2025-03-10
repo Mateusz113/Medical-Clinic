0843cc9c1723a777032c9aaa3c675a6993d40111
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-03-10 13:57:36 +0100

    Added JsonView annotations to controllers

--- src/main/java/com/example/demo/controller/FacilityController.java
+++ src/main/java/com/example/demo/controller/FacilityController.java
@@ -1,8 +1,11 @@
 package com.example.demo.controller;
 
+import com.example.demo.json_view.DoctorJsonViews;
+import com.example.demo.json_view.FacilityJsonViews;
 import com.example.demo.model.facility.FacilityDTO;
 import com.example.demo.model.facility.FullFacilityDataDTO;
 import com.example.demo.service.FacilityService;
+import com.fasterxml.jackson.annotation.JsonView;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -25,21 +28,25 @@ public class FacilityController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
+    @JsonView(FacilityJsonViews.Partial.class)
     public FacilityDTO createFacility(@RequestBody FullFacilityDataDTO facilityData) {
         return facilityService.createFacility(facilityData);
     }
 
     @GetMapping
+    @JsonView(FacilityJsonViews.Partial.class)
     public List<FacilityDTO> getFacilities() {
         return facilityService.getFacilities();
     }
 
     @GetMapping("/{id}")
+    @JsonView(FacilityJsonViews.Partial.class)
     public FacilityDTO getFacilityById(@PathVariable("id") Long id) {
         return facilityService.getFacilityById(id);
     }
 
     @PutMapping("/{id}")
+    @JsonView(FacilityJsonViews.Partial.class)
     public FacilityDTO editFacility(@PathVariable("id") Long id, @RequestBody FullFacilityDataDTO facilityData) {
         return facilityService.editFacility(id, facilityData);
     }
