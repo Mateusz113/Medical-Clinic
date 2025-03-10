40aa503ffa34712de77155deff6d74b0a26758b5
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-03-10 13:57:07 +0100

    Added JsonViews

---
+++ src/main/java/com/example/demo/json_view/DoctorJsonViews.java
@@ -0,0 +1,9 @@
+package com.example.demo.json_view;
+
+public class DoctorJsonViews {
+    public interface Partial {
+    }
+
+    public interface Standalone extends FacilityJsonViews.Partial {
+    }
+}
---
+++ src/main/java/com/example/demo/json_view/FacilityJsonViews.java
@@ -0,0 +1,9 @@
+package com.example.demo.json_view;
+
+public class FacilityJsonViews {
+    public interface Partial {
+    }
+
+    public interface Standalone extends Partial {
+    }
+}
