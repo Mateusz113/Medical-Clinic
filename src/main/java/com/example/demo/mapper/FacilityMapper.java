19b9158b721d69b1c46beefbb2a955ad2eda94c5
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-03-10 13:40:21 +0100

    Updated mappers
    Reworked to not include specific set fields when mapping (fixes infinite loop)

--- src/main/java/com/example/demo/mapper/FacilityMapper.java
+++ src/main/java/com/example/demo/mapper/FacilityMapper.java
@@ -1,26 +1,22 @@
 package com.example.demo.mapper;
 
 import com.example.demo.model.doctor.Doctor;
-import com.example.demo.model.doctor.DoctorDataDTO;
+import com.example.demo.model.doctor.DoctorDTO;
 import com.example.demo.model.facility.Facility;
 import com.example.demo.model.facility.FacilityDTO;
-import com.example.demo.model.facility.FacilityDataDTO;
 import com.example.demo.model.facility.FullFacilityDataDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface FacilityMapper {
+    @Mapping(target = "doctors.facilities", ignore = true)
     FacilityDTO toDTO(Facility facility);
-
-    FacilityDataDTO toDataDTO(Facility facility);
 
     @Mapping(target = "id", expression = "java(null)")
     @Mapping(target = "doctors", expression = "java(new java.util.HashSet<>())")
     Facility toEntity(FullFacilityDataDTO facilityData);
 
-    default DoctorDataDTO doctorToDoctorDataDTO(Doctor doctor) {
-        return Mappers.getMapper(DoctorMapper.class).toDataDTO(doctor);
-    }
+    @Mapping(target = "facilities", ignore = true)
+    DoctorDTO getDoctorDTO(Doctor doctor);
 }
