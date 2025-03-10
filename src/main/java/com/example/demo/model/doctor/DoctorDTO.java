c072f4a1e74a0c602962f4d0a2389fd0f244101e
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-03-10 13:57:19 +0100

    Added JsonView annotations

--- src/main/java/com/example/demo/model/doctor/DoctorDTO.java
+++ src/main/java/com/example/demo/model/doctor/DoctorDTO.java
@@ -1,15 +1,24 @@
 package com.example.demo.model.doctor;
 
+import com.example.demo.json_view.DoctorJsonViews;
+import com.example.demo.json_view.FacilityJsonViews;
 import com.example.demo.model.facility.FacilityDTO;
+import com.fasterxml.jackson.annotation.JsonView;
 
 import java.util.Set;
 
 public record DoctorDTO(
+        @JsonView({DoctorJsonViews.Partial.class, FacilityJsonViews.Partial.class})
         Long id,
+        @JsonView({DoctorJsonViews.Partial.class, FacilityJsonViews.Partial.class})
         String email,
+        @JsonView({DoctorJsonViews.Partial.class, FacilityJsonViews.Partial.class})
         String firstName,
+        @JsonView({DoctorJsonViews.Partial.class, FacilityJsonViews.Partial.class})
         String lastName,
+        @JsonView({DoctorJsonViews.Partial.class, FacilityJsonViews.Partial.class})
         String specialization,
+        @JsonView({DoctorJsonViews.Partial.class, FacilityJsonViews.Standalone.class})
         Set<FacilityDTO> facilities
 ) {
 }
