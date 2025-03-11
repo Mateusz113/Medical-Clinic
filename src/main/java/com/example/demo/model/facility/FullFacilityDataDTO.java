964ae0ae7cc3cdb4eefb555118b77a4589486d07
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-03-12 00:58:44 +0100

    Update FullFacilityDataDTO.java
    Added a new doctors data to creational DTO

--- src/main/java/com/example/demo/model/facility/FullFacilityDataDTO.java
+++ src/main/java/com/example/demo/model/facility/FullFacilityDataDTO.java
@@ -1,10 +1,15 @@
 package com.example.demo.model.facility;
 
+import com.example.demo.model.doctor.FullDoctorDataDTO;
+
+import java.util.Set;
+
 public record FullFacilityDataDTO(
         String name,
         String city,
         String zipCode,
         String street,
-        String buildingNumber
+        String buildingNumber,
+        Set<FullDoctorDataDTO> doctors
 ) {
 }
