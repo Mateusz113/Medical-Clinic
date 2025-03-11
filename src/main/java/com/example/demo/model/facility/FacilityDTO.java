ff951c11c3885577f59ee767756d0d594ef046f5
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-03-11 13:47:22 +0100

    Removed json views and added simple dtos

--- src/main/java/com/example/demo/model/facility/FacilityDTO.java
+++ src/main/java/com/example/demo/model/facility/FacilityDTO.java
@@ -1,26 +1,16 @@
 package com.example.demo.model.facility;
 
-import com.example.demo.json_view.DoctorJsonViews;
-import com.example.demo.json_view.FacilityJsonViews;
-import com.example.demo.model.doctor.DoctorDTO;
-import com.fasterxml.jackson.annotation.JsonView;
+import com.example.demo.model.doctor.SimpleDoctorDTO;
 
 import java.util.Set;
 
 public record FacilityDTO(
-        @JsonView({DoctorJsonViews.Partial.class, FacilityJsonViews.Partial.class})
         Long id,
-        @JsonView({DoctorJsonViews.Partial.class, FacilityJsonViews.Partial.class})
         String name,
-        @JsonView({DoctorJsonViews.Partial.class, FacilityJsonViews.Partial.class})
         String city,
-        @JsonView({DoctorJsonViews.Partial.class, FacilityJsonViews.Partial.class})
         String zipCode,
-        @JsonView({DoctorJsonViews.Partial.class, FacilityJsonViews.Partial.class})
         String street,
-        @JsonView({DoctorJsonViews.Partial.class, FacilityJsonViews.Partial.class})
         String buildingNumber,
-        @JsonView({DoctorJsonViews.Standalone.class, FacilityJsonViews.Partial.class})
-        Set<DoctorDTO> doctors
+        Set<SimpleDoctorDTO> doctors
 ) {
 }
