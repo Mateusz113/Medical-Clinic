b1306f45862f9aa8ee4514665933a7aa85981791
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-03-11 13:48:02 +0100

    Updated mappers with Simple dto classes

--- src/main/java/com/example/demo/mapper/DoctorMapper.java
+++ src/main/java/com/example/demo/mapper/DoctorMapper.java
@@ -3,19 +3,23 @@ package com.example.demo.mapper;
 import com.example.demo.model.doctor.Doctor;
 import com.example.demo.model.doctor.DoctorDTO;
 import com.example.demo.model.doctor.FullDoctorDataDTO;
+import com.example.demo.model.doctor.SimpleDoctorDTO;
 import com.example.demo.model.facility.Facility;
-import com.example.demo.model.facility.FacilityDTO;
+import com.example.demo.model.facility.SimpleFacilityDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface DoctorMapper {
     DoctorDTO toDTO(Doctor doctor);
 
+    SimpleDoctorDTO toSimpleDTO(Doctor doctor);
+
     @Mapping(target = "id", expression = "java(null)")
-    @Mapping(target = "facilities", expression = "java(new java.util.HashSet<>())")
     Doctor toEntity(FullDoctorDataDTO doctorData);
 
-    @Mapping(target = "doctors", ignore = true)
-    FacilityDTO getFacilityDto(Facility facility);
+    default SimpleFacilityDTO getSimpleFacilityDTO(Facility facility) {
+        return Mappers.getMapper(FacilityMapper.class).toSimpleDTO(facility);
+    }
 }
--- src/main/java/com/example/demo/mapper/FacilityMapper.java
+++ src/main/java/com/example/demo/mapper/FacilityMapper.java
@@ -1,22 +1,25 @@
 package com.example.demo.mapper;
 
 import com.example.demo.model.doctor.Doctor;
-import com.example.demo.model.doctor.DoctorDTO;
+import com.example.demo.model.doctor.SimpleDoctorDTO;
 import com.example.demo.model.facility.Facility;
 import com.example.demo.model.facility.FacilityDTO;
 import com.example.demo.model.facility.FullFacilityDataDTO;
+import com.example.demo.model.facility.SimpleFacilityDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface FacilityMapper {
-    @Mapping(target = "doctors.facilities", ignore = true)
     FacilityDTO toDTO(Facility facility);
 
+    SimpleFacilityDTO toSimpleDTO(Facility facility);
+
     @Mapping(target = "id", expression = "java(null)")
-    @Mapping(target = "doctors", expression = "java(new java.util.HashSet<>())")
     Facility toEntity(FullFacilityDataDTO facilityData);
 
-    @Mapping(target = "facilities", ignore = true)
-    DoctorDTO getDoctorDTO(Doctor doctor);
+    default SimpleDoctorDTO getSimpleDoctorDTO(Doctor doctor) {
+        return Mappers.getMapper(DoctorMapper.class).toSimpleDTO(doctor);
+    }
 }
