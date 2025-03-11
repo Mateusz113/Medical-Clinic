c1365be0ca213fdf72c7f0ce353814de72407c8f
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-03-12 00:58:56 +0100

    Update DoctorService.java
    Added DoctorValidator

--- src/main/java/com/example/demo/service/DoctorService.java
+++ src/main/java/com/example/demo/service/DoctorService.java
@@ -1,11 +1,8 @@
 package com.example.demo.service;
 
-import com.example.demo.exception.doctor.DoctorAlreadyExistsException;
 import com.example.demo.exception.doctor.DoctorFacilityContractViolationException;
-import com.example.demo.exception.doctor.DoctorIllegalArgumentException;
 import com.example.demo.exception.doctor.DoctorNotFoundException;
 import com.example.demo.exception.facility.FacilityNotFoundException;
-import com.example.demo.exception.patient.PatientAlreadyExistsException;
 import com.example.demo.mapper.DoctorMapper;
 import com.example.demo.model.doctor.Doctor;
 import com.example.demo.model.doctor.DoctorDTO;
@@ -13,13 +10,13 @@ import com.example.demo.model.doctor.FullDoctorDataDTO;
 import com.example.demo.model.facility.Facility;
 import com.example.demo.repository.DoctorRepository;
 import com.example.demo.repository.FacilityRepository;
+import com.example.demo.validator.DoctorValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.OffsetDateTime;
 import java.util.List;
-import java.util.Objects;
 import java.util.function.BiFunction;
 
 @Service
@@ -28,10 +25,11 @@ public class DoctorService {
     private final DoctorRepository doctorRepository;
     private final FacilityRepository facilityRepository;
     private final DoctorMapper doctorMapper;
+    private final DoctorValidator doctorValidator;
 
     @Transactional
     public DoctorDTO createDoctor(FullDoctorDataDTO doctorData) {
-        validateDoctorCreation(doctorData);
+        doctorValidator.validateDoctorCreation(doctorData);
         Doctor doctor = doctorRepository.save(doctorMapper.toEntity(doctorData));
         return doctorMapper.toDTO(doctor);
     }
@@ -50,7 +48,7 @@ public class DoctorService {
     @Transactional
     public DoctorDTO editDoctor(String email, FullDoctorDataDTO doctorData) {
         Doctor doctor = getDoctorWithEmail(email);
-        validateDoctorEdit(doctor, doctorData);
+        doctorValidator.validateDoctorEdit(doctor, doctorData);
         doctor.update(doctorData);
         doctorRepository.save(doctor);
         return doctorMapper.toDTO(doctor);
@@ -88,33 +86,5 @@ public class DoctorService {
     private Doctor getDoctorWithEmail(String email) {
         return doctorRepository.findByEmail(email)
                 .orElseThrow(() -> new DoctorNotFoundException("Doctor with email: %s does not exist.".formatted(email), OffsetDateTime.now()));
-    }
-
-    private void validateDoctorData(FullDoctorDataDTO doctorData) {
-        if (doctorData.email() == null
-                || doctorData.password() == null
-                || doctorData.firstName() == null
-                || doctorData.lastName() == null
-                || doctorData.specialization() == null) {
-            throw new DoctorIllegalArgumentException("There cannot be null fields in doctor.", OffsetDateTime.now());
-        }
-    }
-
-    private void validateEmailAvailability(String email) {
-        if (doctorRepository.existsByEmail(email)) {
-            throw new DoctorAlreadyExistsException("Doctor with email: %s already exists.".formatted(email), OffsetDateTime.now());
-        }
-    }
-
-    private void validateDoctorEdit(Doctor doctor, FullDoctorDataDTO doctorData) {
-        validateDoctorData(doctorData);
-        if (doctorRepository.existsByEmail(doctorData.email()) && !Objects.equals(doctorData.email(), doctor.getEmail())) {
-            throw new PatientAlreadyExistsException("Doctor with email: %s already exists.".formatted(doctorData.email()), OffsetDateTime.now());
-        }
-    }
-
-    private void validateDoctorCreation(FullDoctorDataDTO doctorData) {
-        validateDoctorData(doctorData);
-        validateEmailAvailability(doctorData.email());
     }
 }
