09f1ee7704089cef6b84619df1c79b1c3cb4f981
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-03-17 16:15:24 +0100

    Update FacilityMapper.java
    Added doctor mapper

--- src/main/java/com/example/demo/mapper/FacilityMapper.java
+++ src/main/java/com/example/demo/mapper/FacilityMapper.java
@@ -1,19 +1,15 @@
 package com.example.demo.mapper;
 
-import com.example.demo.model.doctor.Doctor;
-import com.example.demo.model.doctor.FullDoctorDataDTO;
-import com.example.demo.model.doctor.SimpleDoctorDTO;
 import com.example.demo.model.facility.Facility;
 import com.example.demo.model.facility.FacilityDTO;
 import com.example.demo.model.facility.FullFacilityDataDTO;
 import com.example.demo.model.facility.SimpleFacilityDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = DoctorMapper.class)
 public interface FacilityMapper {
     FacilityDTO toDTO(Facility facility);
 
@@ -23,12 +19,4 @@ public interface FacilityMapper {
 
     @Mapping(target = "id", expression = "java(null)")
     Facility toEntity(FullFacilityDataDTO facilityData);
-
-    default SimpleDoctorDTO getSimpleDoctorDTO(Doctor doctor) {
-        return Mappers.getMapper(DoctorMapper.class).toSimpleDTO(doctor);
-    }
-
-    default Doctor getDoctor(FullDoctorDataDTO doctorData) {
-        return Mappers.getMapper(DoctorMapper.class).toEntity(doctorData);
-    }
 }
