38de4ed9e7d95a972b47bd18795a55986fa3b9c9
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-03-17 17:59:57 +0100

    Update FacilityRepository.java
    Added a method to retireve list of facilities that match the list of ids

--- src/main/java/com/example/demo/repository/FacilityRepository.java
+++ src/main/java/com/example/demo/repository/FacilityRepository.java
@@ -2,9 +2,15 @@ package com.example.demo.repository;
 
 import com.example.demo.model.facility.Facility;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface FacilityRepository extends JpaRepository<Facility, Long> {
     boolean existsByName(String name);
+
+    @Query("select f from FACILITIES f where f.id in :ids")
+    List<Facility> findFacilitiesByIds(List<Long> ids);
 }
