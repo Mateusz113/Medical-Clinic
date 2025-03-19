10a9a92d40c354b3fa5eaa6ff61f0bdaa6a16c88
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-03-19 13:27:53 +0100

    Create VisitRepository.java

---
+++ src/main/java/com/example/demo/repository/VisitRepository.java
@@ -0,0 +1,26 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.doctor.Doctor;
+import com.example.demo.model.visit.Visit;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.time.OffsetDateTime;
+
+@Repository
+public interface VisitRepository extends JpaRepository<Visit, Long> {
+    @Query("select count(v) > 0 from VISITS v where v.startTime <= :endTime and v.endTime >= :startTime and v.doctor = :doctor")
+    boolean existsBetweenDatesInclusive(OffsetDateTime startTime, OffsetDateTime endTime, Doctor doctor);
+
+    @Modifying
+    @Query("update VISITS v set v.patient = null where v.patient.id = :patientId")
+    void detachPatientIdFromVisits(Long patientId);
+
+    Page<Visit> findAllByDoctorId(Long doctorId, Pageable pageable);
+
+    Page<Visit> findAllByPatientId(Long patientId, Pageable pageable);
+}
