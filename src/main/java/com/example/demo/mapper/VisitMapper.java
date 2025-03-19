44789e0f19509c205d5ffdb378135d529968ad8d
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-03-19 13:27:48 +0100

    Create VisitMapper.java

---
+++ src/main/java/com/example/demo/mapper/VisitMapper.java
@@ -0,0 +1,17 @@
+package com.example.demo.mapper;
+
+import com.example.demo.model.visit.Visit;
+import com.example.demo.model.visit.VisitCreationDTO;
+import com.example.demo.model.visit.VisitDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring", uses = {PatientMapper.class, DoctorMapper.class})
+public interface VisitMapper {
+    VisitDTO toDto(Visit visit);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "patient", ignore = true)
+    @Mapping(target = "doctor", ignore = true)
+    Visit toEntity(VisitCreationDTO visitCreationDTO);
+}
