64e7f31dab901fb76e259ef2383f9b4ce56f2688
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-03-21 14:20:58 +0100

    Update Patient.java
    Added builder

--- src/main/java/com/example/demo/model/patient/Patient.java
+++ src/main/java/com/example/demo/model/patient/Patient.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,10 +16,12 @@ import java.time.LocalDate;
 import java.util.Objects;
 
 @Entity(name = "PATIENTS")
+@Builder
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
+@AllArgsConstructor
 public class Patient {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
