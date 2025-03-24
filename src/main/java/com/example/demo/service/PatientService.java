3a063af816f17df8837d1c627c77aa06b9e3fb0e
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-03-25 00:42:10 +0100

    Update PatientService.java
    Moved validation to validator
    Added a clock
    Fixed methods order
    Added calls to validator
    Added command

--- src/main/java/com/example/demo/service/PatientService.java
+++ src/main/java/com/example/demo/service/PatientService.java
@@ -1,23 +1,22 @@
 package com.example.demo.service;
 
-import com.example.demo.exception.patient.PatientAlreadyExistsException;
-import com.example.demo.exception.patient.PatientIllegalDataException;
+import com.example.demo.command.patient.UpsertPatientCommand;
 import com.example.demo.exception.patient.PatientNotFoundException;
 import com.example.demo.mapper.PatientMapper;
 import com.example.demo.model.PageableContentDto;
-import com.example.demo.model.patient.FullPatientDataDTO;
 import com.example.demo.model.patient.Patient;
 import com.example.demo.model.patient.PatientDTO;
 import com.example.demo.repository.PatientRepository;
 import com.example.demo.repository.VisitRepository;
+import com.example.demo.validator.PatientValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Clock;
 import java.time.OffsetDateTime;
-import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -25,52 +24,46 @@ public class PatientService {
     private final PatientRepository patientRepository;
     private final PatientMapper patientMapper;
     private final VisitRepository visitRepository;
+    private final Clock clock;
+
+    @Transactional
+    public PatientDTO createPatient(UpsertPatientCommand upsertPatientCommand) {
+        PatientValidator.validatePatientCreation(upsertPatientCommand, patientRepository, clock);
+        Patient patient = patientRepository.save(patientMapper.toEntity(upsertPatientCommand));
+        return patientMapper.toDTO(patient);
+    }
 
     public PageableContentDto<PatientDTO> getAllPatients(Pageable pageable) {
         return getAllPatientsWithPageable(pageable);
     }
 
     public PatientDTO getPatient(String email) {
-        Patient patient = patientRepository.findByEmail(email)
-                .orElseThrow(() -> new PatientNotFoundException("Patient with email: %s does not exist.".formatted(email), OffsetDateTime.now()));
+        Patient patient = getPatientByEmail(email);
         return patientMapper.toDTO(patient);
     }
 
     @Transactional
-    public PatientDTO createPatient(FullPatientDataDTO patientData) {
-        validateFullPatientDataDTO(patientData);
-        validateEmailAvailability(patientData.email());
-        Patient patient = patientRepository.save(patientMapper.toEntity(patientData));
-        return patientMapper.toDTO(patient);
-    }
-
-    @Transactional
-    public void deletePatient(String email) {
-        Patient patient = patientRepository.findByEmail(email)
-                .orElseThrow(() -> new PatientNotFoundException("Patient with email: %s does not exist.".formatted(email), OffsetDateTime.now()));
-        visitRepository.detachPatientIdFromVisits(patient.getId());
-        patientRepository.delete(patient);
-    }
-
-    @Transactional
-    public PatientDTO editPatient(String email, FullPatientDataDTO patientData) {
-        Patient patient = patientRepository.findByEmail(email)
-                .orElseThrow(() -> new PatientNotFoundException("Patient with email %s does not exist.".formatted(email), OffsetDateTime.now()));
-        validatePatientEdit(patient, patientData);
-        patient.update(patientData);
+    public PatientDTO editPatient(String email, UpsertPatientCommand upsertPatientCommand) {
+        Patient patient = getPatientByEmail(email);
+        PatientValidator.validatePatientEdit(patient, upsertPatientCommand, patientRepository, clock);
+        patient.update(upsertPatientCommand);
         patientRepository.save(patient);
         return patientMapper.toDTO(patient);
     }
 
     @Transactional
     public void editPatientPassword(String email, String password) {
-        if (password == null) {
-            throw new PatientIllegalDataException("Password cannot be set to null.", OffsetDateTime.now());
-        }
-        Patient patient = patientRepository.findByEmail(email)
-                .orElseThrow(() -> new PatientNotFoundException("Patient with email %s does not exist.".formatted(email), OffsetDateTime.now()));
+        PatientValidator.validatePatientPasswordEdit(password, clock);
+        Patient patient = getPatientByEmail(email);
         patient.setPassword(password);
         patientRepository.save(patient);
+    }
+
+    @Transactional
+    public void deletePatient(String email) {
+        Patient patient = getPatientByEmail(email);
+        visitRepository.detachPatientIdFromVisits(patient.getId());
+        patientRepository.delete(patient);
     }
 
     private PageableContentDto<PatientDTO> getAllPatientsWithPageable(Pageable pageable) {
@@ -83,31 +76,8 @@ public class PatientService {
                 .build();
     }
 
-    private void validateFullPatientDataDTO(FullPatientDataDTO patientData) {
-        if (patientData.email() == null
-                || patientData.password() == null
-                || patientData.idCardNo() == null
-                || patientData.firstName() == null
-                || patientData.lastName() == null
-                || patientData.phoneNumber() == null
-                || patientData.birthday() == null) {
-            throw new PatientIllegalDataException("There cannot be null fields in patient data.", OffsetDateTime.now());
-        }
-    }
-
-    private void validateEmailAvailability(String email) {
-        if (patientRepository.existsByEmail(email)) {
-            throw new PatientAlreadyExistsException("Patient with email: %s already exists.".formatted(email), OffsetDateTime.now());
-        }
-    }
-
-    private void validatePatientEdit(Patient patient, FullPatientDataDTO patientData) {
-        validateFullPatientDataDTO(patientData);
-        if (patientRepository.existsByEmail(patientData.email()) && !Objects.equals(patient.getEmail(), patientData.email())) {
-            throw new PatientAlreadyExistsException("Patient with email: %s already exists.".formatted(patientData.email()), OffsetDateTime.now());
-        }
-        if (!patient.getIdCardNo().equals(patientData.idCardNo())) {
-            throw new PatientIllegalDataException("ID card number cannot be changed.", OffsetDateTime.now());
-        }
+    private Patient getPatientByEmail(String email) {
+        return patientRepository.findByEmail(email)
+                .orElseThrow(() -> new PatientNotFoundException("Patient with email: %s does not exist.".formatted(email), OffsetDateTime.now(clock)));
     }
 }
