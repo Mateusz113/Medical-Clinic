d264196da74812f2a2a75b33b311395cc8cfd35c
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-03-25 15:16:51 +0100

    Update Facility.java
    Added builder

--- src/main/java/com/example/demo/model/facility/Facility.java
+++ src/main/java/com/example/demo/model/facility/Facility.java
@@ -8,6 +8,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -17,9 +19,11 @@ import java.util.Objects;
 import java.util.Set;
 
 @Entity(name = "FACILITIES")
-@NoArgsConstructor
+@Builder
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Facility {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
