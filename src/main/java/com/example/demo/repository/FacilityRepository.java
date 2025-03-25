3c4c2c821118222acdf82dcd47a82ad7ff18173f
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-03-25 15:44:17 +0100

    Update FacilityRepository.java

--- src/main/java/com/example/demo/repository/FacilityRepository.java
+++ src/main/java/com/example/demo/repository/FacilityRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Set;
 
 @Repository
 public interface FacilityRepository extends JpaRepository<Facility, Long> {
