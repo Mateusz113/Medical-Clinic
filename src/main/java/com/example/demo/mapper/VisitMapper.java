e83e5b7cab4c50f0ff09e33b27d254bb0f2a4263
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-03-26 13:09:19 +0100

    Update VisitMapper.java
    Removed uses as it was breaking tests
    Added command instead of dto

--- src/main/java/com/example/demo/mapper/VisitMapper.java
+++ src/main/java/com/example/demo/mapper/VisitMapper.java
@@ -1,17 +1,17 @@
 package com.example.demo.mapper;
 
+import com.example.demo.command.visit.InsertVisitCommand;
 import com.example.demo.model.visit.Visit;
-import com.example.demo.model.visit.VisitCreationDTO;
 import com.example.demo.model.visit.VisitDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring", uses = {PatientMapper.class, DoctorMapper.class})
+@Mapper(componentModel = "spring")
 public interface VisitMapper {
     VisitDTO toDto(Visit visit);
 
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "patient", ignore = true)
     @Mapping(target = "doctor", ignore = true)
-    Visit toEntity(VisitCreationDTO visitCreationDTO);
+    Visit toEntity(InsertVisitCommand insertVisitCommand);
 }
