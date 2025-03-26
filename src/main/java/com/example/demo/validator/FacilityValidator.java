3e21a16bf8ea87637739e94c7370027ff200a620
Mateusz Chilicki <mateusz.ch.113@gmail.com>
2025-03-26 17:04:38 +0100

    Update FacilityValidator.java
    Added clock and command

--- src/main/java/com/example/demo/validator/FacilityValidator.java
+++ src/main/java/com/example/demo/validator/FacilityValidator.java
@@ -1,41 +1,42 @@
 package com.example.demo.validator;
 
+import com.example.demo.command.facility.UpsertFacilityCommand;
 import com.example.demo.exception.facility.FacilityAlreadyExistsException;
 import com.example.demo.exception.facility.FacilityIllegalDataException;
 import com.example.demo.model.facility.Facility;
-import com.example.demo.model.facility.FullFacilityDataDTO;
 import com.example.demo.repository.FacilityRepository;
 
+import java.time.Clock;
 import java.time.OffsetDateTime;
 import java.util.Objects;
 
 public class FacilityValidator {
-    public static void validateFacilityCreation(FullFacilityDataDTO facilityData, FacilityRepository facilityRepository) {
-        validateFacilityData(facilityData);
-        validateNameAvailability(facilityData.name(), facilityRepository);
-        DoctorValidator.validateDoctorBulkCreation(facilityData.doctors());
+    public static void validateFacilityCreation(UpsertFacilityCommand upsertFacilityCommand, FacilityRepository facilityRepository, Clock clock) {
+        validateFacilityData(upsertFacilityCommand, clock);
+        validateNameAvailability(upsertFacilityCommand.name(), facilityRepository, clock);
+        DoctorValidator.validateDoctorBulkCreation(upsertFacilityCommand.doctors(), clock);
     }
 
-    public static void validateFacilityEdit(Facility facility, FullFacilityDataDTO facilityData, FacilityRepository facilityRepository) {
-        validateFacilityData(facilityData);
-        if (facilityRepository.existsByName(facilityData.name()) && !Objects.equals(facility.getName(), facilityData.name())) {
-            throw new FacilityAlreadyExistsException("Facility with name: %s already exists.".formatted(facilityData.name()), OffsetDateTime.now());
+    public static void validateFacilityEdit(Facility facility, UpsertFacilityCommand upsertFacilityCommand, FacilityRepository facilityRepository, Clock clock) {
+        validateFacilityData(upsertFacilityCommand, clock);
+        if (facilityRepository.existsByName(upsertFacilityCommand.name()) && !Objects.equals(facility.getName(), upsertFacilityCommand.name())) {
+            throw new FacilityAlreadyExistsException("Facility with name: %s already exists.".formatted(upsertFacilityCommand.name()), OffsetDateTime.now(clock));
         }
     }
 
-    private static void validateFacilityData(FullFacilityDataDTO facilityData) {
-        if (Objects.isNull(facilityData.name())
-                || Objects.isNull(facilityData.city())
-                || Objects.isNull(facilityData.zipCode())
-                || Objects.isNull(facilityData.street())
-                || Objects.isNull(facilityData.buildingNumber())) {
-            throw new FacilityIllegalDataException("There cannot be null fields in facility.", OffsetDateTime.now());
+    private static void validateFacilityData(UpsertFacilityCommand upsertFacilityCommand, Clock clock) {
+        if (Objects.isNull(upsertFacilityCommand.name())
+                || Objects.isNull(upsertFacilityCommand.city())
+                || Objects.isNull(upsertFacilityCommand.zipCode())
+                || Objects.isNull(upsertFacilityCommand.street())
+                || Objects.isNull(upsertFacilityCommand.buildingNumber())) {
+            throw new FacilityIllegalDataException("There cannot be null fields in facility.", OffsetDateTime.now(clock));
         }
     }
 
-    private static void validateNameAvailability(String name, FacilityRepository facilityRepository) {
+    private static void validateNameAvailability(String name, FacilityRepository facilityRepository, Clock clock) {
         if (facilityRepository.existsByName(name)) {
-            throw new FacilityAlreadyExistsException("Facility with name: %s already exists.".formatted(name), OffsetDateTime.now());
+            throw new FacilityAlreadyExistsException("Facility with name: %s already exists.".formatted(name), OffsetDateTime.now(clock));
         }
     }
 }
